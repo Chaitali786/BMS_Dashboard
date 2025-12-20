3ba2561c266188064f300aa3ad58b1b06c2b503f
Chaitali Deore <chaitalideore@gmail.com>
2025-12-20 10:26:39 +0100

    ui design improvement

--- bms-dashboard/app/page.tsx
+++ bms-dashboard/app/page.tsx
@@ -3,11 +3,12 @@ import Telemetry from "./components/telemetry_form";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-       
-       <Telemetry />
-       
-      </main>
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-4">
+      <h1 className="text-emerald-500 font-bold text-3xl mb-8 text-center">
+        Batteries Made In Sweden
+      </h1>
+      <Telemetry />
+    </main>
     </div>
   );
 }
